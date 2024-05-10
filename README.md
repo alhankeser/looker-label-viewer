6b3117e90fede5b69a1430d3e70c5767a1b46667
Alhan Keser <alhankeser@gmail.com>
2024-05-10 23:46:58 +0200

    First working version with no customization

---
+++ README.md
@@ -0,0 +1,20 @@
+# Looker Label Viewer for VS Code
+
+Looker Label Viewer allows you to see localized labels in lookml files without having to open the localization file.
+
+## Features
+
+Hover over a localized label to view the corresponding label value.
+
+It currently does not support the ability to change extension settings related to:
+- target localization file. It defaults to `en.strings.json`)
+- Target attributes. It defaults to trying to get labels for:
+  - label
+  - group_label
+  - group_item_label
+  - view_label
+
+### 0.0.1
+
+Initial working release with no customization options.
+
---
+++ extension.js
@@ -0,0 +1,61 @@
+const vscode = require('vscode');
+let labelLookup = {};
+
+function watchHover() {
+	vscode.languages.registerHoverProvider('lookml', {
+		provideHover(document, position) {
+			const lineText = document.lineAt(position.line).b;
+			const splitText = lineText.split(':');
+			const key = splitText[0].trim();
+			const val = splitText.slice(1).join(':').trim().replace(/['"]+/g, '');
+			
+			if (['label', 'view_label', 'group_label', 'group_item_label'].indexOf(key) > -1) {
+				const result = labelLookup[val];
+				if (result == undefined) {
+					return {
+						contents: ['Not found in localization file']
+					}
+				}
+				return {
+					contents: [result]
+				};
+			}
+		}
+	  });
+}
+
+function updateLabelLookup() {
+	if (vscode.workspace.workspaceFolders) {
+		vscode.workspace.workspaceFolders.forEach((folder) => {
+			vscode.workspace.findFiles(new vscode.RelativePattern(folder, '**/en.strings.json'), '', 1)
+			.then((uris) => {
+				const uri = uris[0];
+				vscode.workspace.fs.readFile(uri).then((result) => {
+					const decoder = new TextDecoder('utf-8');
+					const fileText = decoder.decode(result);
+					const jsonObject = JSON.parse(fileText);
+					labelLookup = jsonObject;
+				})
+			});
+		});
+	}
+}
+
+function activate() {
+	vscode.window.showInformationMessage('Looker Label Viewer is now active!');
+	updateLabelLookup();
+	watchHover();
+}
+
+vscode.workspace.onDidChangeTextDocument(event => {
+	if (event.document.uri.path.endsWith('strings.json')) {
+		updateLabelLookup()
+	}
+});
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
